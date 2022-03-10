9e4aaf63003da952beb5be8d61150d43e90c8c89
Hyukjoo_Lee <hjjaydon.lee@gmail.com>
2022-03-10 15:36:22 -0800

    second test

--- bmi.js
+++ bmi.js
@@ -2,7 +2,6 @@
 const express = require("express");
 
 const app = express();
-const port = 3000;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
@@ -42,6 +41,10 @@ app.post("/", function (req, res) {
   });
 });
 
-app.listen(port, function () {
-  console.log("Listening port " + port);
+app.listen(process.env.PORT || 3000, function () {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
